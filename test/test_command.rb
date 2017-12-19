4766ea794894841be589be70fe6880d04d307c74
Brian L. McMichael <brian@brianmcmichael.com>
2017-12-19 16:24:40 -0500

    Update test to expected values

--- test/test_command.rb
+++ test/test_command.rb
@@ -4,21 +4,19 @@ require 'command'
 class TestCommand < Minitest::Test
 
   def test_command_output_parsing
-    output = <<-EOF
-
-efranz    30328  0.1  0.1 462148 28128 ?        Sl   20:28   0:00 Passenger RackApp: /users/PZS0562/efranz/awesim/dev/ood-example-ps
-
-EOF
+    output = File.read("test/sacct.txt")
     processes = Command.new.parse(output)
 
-    assert_equal 1, processes.count
+    #assert_equal 1, processes.count
 
     p = processes.first
 
-    assert_equal "efranz", p.user
-    assert_equal "462148", p.vsz
-    assert_equal "28128", p.rss
-    assert_equal "0:00", p.time
-    assert_equal "Passenger RackApp: /users/PZS0562/efranz/awesim/dev/ood-example-ps", p.command
+    assert_equal "1933171", p.jobid
+    assert_equal "pbe_fe_2_+", p.jobname
+    assert_equal "RM-shared", p.partition
+    assert_equal "mr4s95p", p.account
+    assert_equal "28", p.cpus
+    assert_equal "RUNNING", p.state
+    assert_equal "0:0", p.exit_code
   end
 end
