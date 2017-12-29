d5e91a4d386e620e2b5620955ac7ac532fad6fa2
Brian L. McMichael <brian@brianmcmichael.com>
2017-12-29 14:13:20 -0500

    Update test with parseable output

--- test/test_command.rb
+++ test/test_command.rb
@@ -8,15 +8,17 @@ class TestCommand < Minitest::Test
     processes = Command.new.parse(output)
 
     #assert_equal 1, processes.count
+    #
+    # 2074737|_mb_bot240520ps_|GPU|mc4sa9p|32|COMPLETED|0:0
 
     p = processes.first
 
-    assert_equal "1933171", p.jobid
-    assert_equal "pbe_fe_2_+", p.jobname
-    assert_equal "RM-shared", p.partition
-    assert_equal "mr4s95p", p.account
-    assert_equal "28", p.cpus
-    assert_equal "RUNNING", p.state
+    assert_equal "2074737", p.jobid
+    assert_equal "_mb_bot240520ps_", p.jobname
+    assert_equal "GPU", p.partition
+    assert_equal "mc4sa9p", p.account
+    assert_equal "32", p.cpus
+    assert_equal "COMPLETED", p.state
     assert_equal "0:0", p.exit_code
   end
 end
