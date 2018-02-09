78994a26b2ce8ff63e8490582c6a083e709ff95b
Brian L. McMichael <brian@brianmcmichael.com>
2018-02-09 10:12:04 -0500

    Update the command for the full jobinfo output

--- command.rb
+++ command.rb
@@ -5,15 +5,20 @@ class Command
 
   def to_s
     # This command is specific to PSC
-    "/opt/packages/slurm/17.02.5/bin/sacct --long -j #{jobid}"
+    "/opt/packages/slurm/17.02.5/bin/sacct --long -P -j #{jobid}"
   end
 
   def initialize(jobid)
     @jobid = jobid
   end
 
+  JobInfo = Struct.new(:JobID, :JobIDRaw, :JobName, :Partition, :MaxVMSize, :MaxVMSizeNode, :MaxVMSizeTask, :AveVMSize, :MaxRSS, :MaxRSSNode, :MaxRSSTask, :AveRSS, :MaxPages, :MaxPagesNode, :MaxPagesTask, :AvePages, :MinCPU, :MinCPUNode, :MinCPUTask, :AveCPU, :NTasks, :AllocCPUS, :Elapsed, :State, :ExitCode, :AveCPUFreq, :ReqCPUFreqMin, :ReqCPUFreqMax, :ReqCPUFreqGov, :ReqMem, :ConsumedEnergy, :MaxDiskRead, :MaxDiskReadNode, :MaxDiskReadTask, :AveDiskRead, :MaxDiskWrite, :MaxDiskWriteNode, :MaxDiskWriteTask, :AveDiskWrite, :AllocGRES, :ReqGRES, :ReqTRES, :AllocTRES )
+
   def parse(output)
-    output
+    lines = output.strip.split("\n").drop(1)
+    lines.map do |line|
+      JobInfo.new(*(line.split("|", 43)))
+    end
   end
 
   # Execute the command, and parse the output, returning and array of
